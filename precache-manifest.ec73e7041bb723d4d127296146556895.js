8a7586bbd9cad203fc1493db00efc7be67f938e6
Nikhita Tithi <nikhita.tithi@gmail.com>
2020-08-09 23:49:12 +0530

    Updates

---
+++ precache-manifest.ec73e7041bb723d4d127296146556895.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3438eb57218ab5ea9f7a5dee12c2793",
+    "url": "/CodaGlobal/index.html"
+  },
+  {
+    "revision": "e4732b0b3db8d6cea98e",
+    "url": "/CodaGlobal/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "bf1704560290a7f18122",
+    "url": "/CodaGlobal/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e4732b0b3db8d6cea98e",
+    "url": "/CodaGlobal/static/js/2.1288686b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/CodaGlobal/static/js/2.1288686b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf1704560290a7f18122",
+    "url": "/CodaGlobal/static/js/main.6cfa4586.chunk.js"
+  },
+  {
+    "revision": "c7eccb680c23a9706c2f",
+    "url": "/CodaGlobal/static/js/runtime-main.54cceeb8.js"
+  }
+]);
